cdf4ef40d7aa1ca7221b2a28172a15b81571cc77
rmead-sfmc <78726115+rmead-sfmc@users.noreply.github.com>
2022-06-30 08:56:12 +0100

    Update cookies.js

--- cookies.js
+++ cookies.js
@@ -1,29 +1,30 @@
-    // System Functions to Manage Cookies
-    function createCookie(name, value, days) {
-      var expires;
-      if (days) {
+ // System Functions to Manage Cookies
+ function createCookie(name, value, days) {
+    var expires;
+
+    if (days) {
         var date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = "; expires=" + date.toGMTString();
-      } else {
+    } else {
         expires = "";
-      }
-      document.cookie = name + "=" + value + expires + "; path=/";
     }
-  
-    function readCookie(name) {
-      var nameEQ = encodeURIComponent(name) + "=";
-      var ca = document.cookie.split(';');
-      for (var i = 0; i < ca.length; i++) {
+    document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = encodeURIComponent(name) + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
         while (c.charAt(0) === ' ')
-          c = c.substring(1, c.length);
+            c = c.substring(1, c.length);
         if (c.indexOf(nameEQ) === 0)
-          return c.replace(/%0d%0a/g, '').substring(nameEQ.length, c.length);
-      }
-      return null;
+            return decodeURIComponent(c.replace(/%0d%0a/g, '').substring(nameEQ.length, c.length));
     }
-  
-    function eraseCookie(name) {
-      createCookie(name, "", -1);
-    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
